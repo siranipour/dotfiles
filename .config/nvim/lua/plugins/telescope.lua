d76c1f4799976312f05a1a42580759b51ce92338
siranipour <s.iranipour@gmail.com>
2024-11-15 12:51:04 +0000

    Formatting

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -16,8 +16,8 @@ return {
                 mappings = {
                     i = {
                         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-                        ["<C-j>"] = actions.move_selection_next, -- move to next result
-                        ["<esc>"] = actions.close, -- exit telescope buffer
+                        ["<C-j>"] = actions.move_selection_next,     -- move to next result
+                        ["<esc>"] = actions.close,                   -- exit telescope buffer
                     },
                 },
             },
