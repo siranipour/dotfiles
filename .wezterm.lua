f9969805a1c8cbeb05f4380ba3438193c4be8b0c
siranipour <s.iranipour@gmail.com>
2024-11-18 14:12:13 +0000

    Explicitly state wezterm font

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'OneDark (base16)'
 
-
+config.font = wezterm.font('JetBrains Mono')
 config.font_size = 14
 
 config.hide_tab_bar_if_only_one_tab = true
