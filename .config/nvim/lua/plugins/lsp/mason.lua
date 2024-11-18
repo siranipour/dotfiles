fb182e324594628b4b8797f194d480793e1af1a7
siranipour <s.iranipour@gmail.com>
2024-11-18 16:44:52 +0000

    Use basedpyright

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -8,23 +8,22 @@ return {
         local mason_lspconfig = require("mason-lspconfig")
 
         mason.setup({
-          ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗",
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
             },
-          },
         })
 
         mason_lspconfig.setup({
-          ensure_installed = {
-            "html",
-            "cssls",
-            "lua_ls",
-            "pyright",
-          },
+            ensure_installed = {
+                "html",
+                "cssls",
+                "lua_ls",
+                "basedpyright",
+            },
         })
-
     end
 }
