d22d8d105a917cca1cd08bd3803b9591c6fcc1d1
siranipour <s.iranipour@gmail.com>
2024-11-19 15:20:23 +0000

    Disable f and t overrides in flash

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -2,7 +2,14 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
-    opts = {},
+    opts = {
+        modes = {
+            char = {
+                enabled = false,
+            },
+        },
+
+    },
     -- stylua: ignore
     keys = {
         { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
