479eddc9a8d2160da98114d8db2940978b6da9bf
siranipour <s.iranipour@gmail.com>
2024-11-19 14:59:41 +0000

    Consistent use of quotes

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,9 +2,9 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'OneDark (base16)'
+config.color_scheme = "OneDark (base16)"
 
-config.font = wezterm.font('JetBrains Mono')
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14
 
 config.hide_tab_bar_if_only_one_tab = true
