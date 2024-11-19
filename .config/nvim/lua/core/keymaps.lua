b1ef5d4f842c4d20e82b79bab23499bad6ba07e5
siranipour <s.iranipour@gmail.com>
2024-11-19 13:26:40 +0000

    Telescope searches hidden files and more keymaps
    * Enable searching of hidden files
        * With the exception of .git node-modules and .venv
    * Add telescope keymaps for search keybindings

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -13,13 +13,13 @@ keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Exit buffer" })
 keymap.set("n", "<ESC><ESC>", "<cmd>nohl<CR>", { desc = "Clear search highlight" })
 
 -- nerd-tree
-keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })                         -- toggle file explorer
 keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })                     -- collapse file explorer
 
 -- telescope file finding
 keymap.set("n", "<leader>ff", "<cmd> Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 keymap.set("n", "<leader>fr", "<cmd> Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 keymap.set("n", "<leader>fs", "<cmd> Telescope live_grep<cr>", { desc = "Find string in cwd" })
 keymap.set("n", "<leader>fc", "<cmd> Telescope grep_string<cr>", { desc = "Find string under cursor" })
-
+keymap.set("n", "<leader>fk", "<cmd> Telescope keymaps<cr>", { desc = "Find keymaps" })
