56a66eb02ab94b48fd02adc4b62318a06bedd2f4
siranipour <s.iranipour@gmail.com>
2024-11-21 18:18:41 +0000

    Change icon for recent projects

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -11,7 +11,7 @@ return {
                     { section = "header" },
                     { section = "keys", gap = 1, padding = 1 },
                     { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-                    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+                    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
                     {
                         pane = 2,
                         icon = " ",
