8c6f2cbc6b9ff307eb360738a33939f862bdd7d8
siranipour <s.iranipour@gmail.com>
2024-12-04 12:22:48 +0000

    Increase gitsigns priority

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,7 @@ return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {
+        sign_priority = 100,
         on_attach = function(bufnr)
             local gs = package.loaded.gitsigns
 
