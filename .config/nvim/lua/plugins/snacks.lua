ac7c63694e2a05405a35c8d14a8ca53f98214424
siranipour <s.iranipour@gmail.com>
2024-12-23 17:14:48 +0000

    Remove redundant animation easing

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -37,7 +37,6 @@ return {
                 enabled = true,
                 animate = {
                     duration = { step = 15, total = 100 },
-                    easing = "linear",
                 },
             },
         })
