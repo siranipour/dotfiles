b1fe6a49162a547ba7fbc7fcb68ef56e0778fa85
siranipour <s.iranipour@gmail.com>
2024-12-28 11:37:26 +0000

    Toggle floating terminal with posix compliant bind

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -15,6 +15,5 @@ return {
         { "<leader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
         { "<leader>r", mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
         { "<leader>R", mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        { "<c-s>",     mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
     },
 }
--- .config/nvim/lua/plugins/fterm.lua
+++ .config/nvim/lua/plugins/fterm.lua
@@ -1,7 +1,7 @@
 return {
     "numToStr/FTerm.nvim",
     config = function()
-        vim.keymap.set('n', '<C-`>', '<CMD>lua require("FTerm").toggle()<CR>', { desc = "Toggle floating terminal" })
-        vim.keymap.set('t', '<C-`>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+        vim.keymap.set('n', '<C-S>', '<CMD>lua require("FTerm").toggle()<CR>', { desc = "Toggle floating terminal" })
+        vim.keymap.set('t', '<C-S>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
     end
 }
