5136ef5bfa58376efdf20b5388f40eeb85418274
siranipour <s.iranipour@gmail.com>
2025-01-12 12:12:39 +0000

    Add call stack up and down keymaps

--- .config/nvim/lua/plugins/dap/nvim-dap.lua
+++ .config/nvim/lua/plugins/dap/nvim-dap.lua
@@ -7,8 +7,10 @@ return {
         vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Debugger continue" })
 		vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Debugger step into" })
 		vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Debugger step over" })
-		vim.keymap.set("n", "<leader>dq", dap.terminate, { desc = "Debugger quit" })
-		vim.keymap.set("n", "<leader>dr", dap.restart, { desc = "Debugger restart" })
 		vim.keymap.set("n", "<leader>du", dap.step_out, { desc = "Debugger step out" })
+		vim.keymap.set("n", "<leader>dq", dap.terminate, { desc = "Debugger quit" })
+		vim.keymap.set("n", "<leader>dk", dap.up, { desc = "Debugger call stack up" })
+		vim.keymap.set("n", "<leader>dj", dap.down, { desc = "Debugger call stack down" })
+		vim.keymap.set("n", "<leader>dr", dap.restart, { desc = "Debugger restart" })
     end,
 }
