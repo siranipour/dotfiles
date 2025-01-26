c37351f7bbb9bc90707ebcb33c23368818fddd02
siranipour <s.iranipour@gmail.com>
2025-01-26 12:19:06 +0000

    Add typr

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -31,6 +31,18 @@ return {
                     },
                     { pane = 2, section = "startup" },
                 },
+                preset = {
+                    keys = {
+                        { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                        { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                        { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                        { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                        { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                        { icon = "", key = "t", desc = "Typr", action = ":Typr"},
+                        { icon = "", key = "T", desc = "Typr Stats", action = ":TyprStats"},
+                        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                    },
+                },
             },
             bigfile = { enabled = true },
             notify = { enabled = true },
