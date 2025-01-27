d2ea7f77a7f00ef7cf3f3bfcc780b985b8a20f2a
siranipour <s.iranipour@gmail.com>
2025-01-27 16:02:43 +0000

    Update auto-save plugin
    (Previous was not actively maintained and did not work with harpoon)

--- .config/nvim/lua/plugins/auto-save.lua
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -1,6 +1,12 @@
 return {
-    'pocco81/auto-save.nvim',
-    config = function()
-        vim.api.nvim_set_keymap("n", "<leader>x", ":ASToggle<CR>", {})
-    end,
+    "okuuva/auto-save.nvim",
+    version = '^1.0.0',
+    cmd = "ASToggle",
+    event = { "InsertLeave", "TextChanged" },
+    keys = {
+        { "<leader>x", "<cmd>ASToggle<CR>", desc = "Toggle auto-save" },
+    },
+    opts = {
+        debounce_delay = 100,
+    },
 }
