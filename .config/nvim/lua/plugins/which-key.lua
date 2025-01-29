53b54ad8bc046abfa90e6640eed5d43b560d43a6
siranipour <s.iranipour@gmail.com>
2025-01-29 15:30:33 +0000

    Update which-key

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,10 +1,8 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 500
-    end,
     opts = {
+        preset = "modern",
+        delay = 1000
     },
 }
