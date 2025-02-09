2aec64e51a01c71629e7c2b540e99c44973d4347
siranipour <s.iranipour@gmail.com>
2025-02-09 13:10:04 +0000

    Remove commented code

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -64,39 +64,5 @@ return {
         vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
         vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
         vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
-
-        -- -- basic telescope configuration
-        -- local conf = require("telescope.config").values
-        -- local function toggle_telescope(harpoon_files)
-        --     local file_paths = {}
-        --     for _, item in ipairs(harpoon_files.items) do
-        --         table.insert(file_paths, item.value)
-        --     end
-        --
-        --     require("telescope.pickers").new({}, {
-        --         prompt_title = "Harpoon",
-        --         finder = require("telescope.finders").new_table({
-        --             results = file_paths,
-        --         }),
-        --         previewer = conf.file_previewer({}),
-        --         sorter = conf.generic_sorter({}),
-        --     }):find()
-        -- end
-        --
-        -- -- open harpoons with splits
-        -- harpoon:extend({
-        --     UI_CREATE = function(cx)
-        --         vim.keymap.set("n", "<C-v>", function()
-        --             harpoon.ui:select_menu_item({ vsplit = true })
-        --         end, { buffer = cx.bufnr })
-        --
-        --         vim.keymap.set("n", "<C-x>", function()
-        --             harpoon.ui:select_menu_item({ split = true })
-        --         end, { buffer = cx.bufnr })
-        --     end,
-        -- })
-        --
-        -- vim.keymap.set("n", "<leader>fh", function() toggle_telescope(harpoon:list()) end,
-        --     { desc = "Open harpoons with telescope" })
     end
 }
