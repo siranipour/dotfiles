608fa0be66feaef8e0003488c3ef93559e93468d
siranipour <s.iranipour@gmail.com>
2025-02-18 22:45:29 +0000

    Change wezterm font to support nerd fonts

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Tokyo Night Moon'
 
 
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14
 
 config.hide_tab_bar_if_only_one_tab = true
