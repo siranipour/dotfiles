b7b8678bf91e6603c52cac0ed29eab8315fb351a
siranipour <s.iranipour@gmail.com>
2025-02-23 12:01:13 +0000

    Rename with lsp when rename files in oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -11,6 +11,14 @@ return {
                 show_hidden = true,
             }
         })
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "OilActionsPost",
+            callback = function(event)
+                if event.data.actions.type == "move" then
+                    require('snacks').rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+                end
+            end,
+        })
         vim.keymap.set("n", "<leader>o", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
     end
 }
