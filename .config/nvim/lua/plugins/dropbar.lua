a5c658a432ba07ad96fa3e0a9ff189f6d9cb2dec
siranipour <s.iranipour@gmail.com>
2025-02-23 12:11:35 +0000

    Update dropbar config

--- .config/nvim/lua/plugins/dropbar.lua
+++ .config/nvim/lua/plugins/dropbar.lua
@@ -1,10 +1,3 @@
 return {
-    {
-        'Bekaboo/dropbar.nvim',
-        -- optional, but required for fuzzy finder support
-        dependencies = {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            build = 'make'
-        }
-    }
+    'Bekaboo/dropbar.nvim',
 }
