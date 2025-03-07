886bd11882347c533c9773c68a674a2968d982bc
siranipour <s.iranipour@gmail.com>
2025-03-07 17:00:57 +0000

    Add conditional breakpoint keymap

--- .config/nvim/lua/plugins/dap/nvim-dap.lua
+++ .config/nvim/lua/plugins/dap/nvim-dap.lua
@@ -4,14 +4,16 @@ return {
         local dap = require("dap")
 
         vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+        vim.keymap.set("n", "<leader>dB", function() dap.toggle_breakpoint(vim.fn.input("Breakpoint condition")) end,
+            { desc = "Toggle conditional breakpoint" })
         vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Debugger continue" })
         vim.keymap.set("n", "<leader>dh", dap.run_to_cursor, { desc = "Debugger run to cursor" })
-		vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Debugger step into" })
-		vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Debugger step over" })
-		vim.keymap.set("n", "<leader>du", dap.step_out, { desc = "Debugger step out" })
-		vim.keymap.set("n", "<leader>dq", dap.terminate, { desc = "Debugger quit" })
-		vim.keymap.set("n", "<leader>dk", dap.up, { desc = "Debugger call stack up" })
-		vim.keymap.set("n", "<leader>dj", dap.down, { desc = "Debugger call stack down" })
-		vim.keymap.set("n", "<leader>dr", dap.restart, { desc = "Debugger restart" })
+        vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Debugger step into" })
+        vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Debugger step over" })
+        vim.keymap.set("n", "<leader>du", dap.step_out, { desc = "Debugger step out" })
+        vim.keymap.set("n", "<leader>dq", dap.terminate, { desc = "Debugger quit" })
+        vim.keymap.set("n", "<leader>dk", dap.up, { desc = "Debugger call stack up" })
+        vim.keymap.set("n", "<leader>dj", dap.down, { desc = "Debugger call stack down" })
+        vim.keymap.set("n", "<leader>dr", dap.restart, { desc = "Debugger restart" })
     end,
 }
