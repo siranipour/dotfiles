b742d516995ece5bf1cf7948f306e80ece01bb2e
Shayan Iranipour <43517072+siranipour@users.noreply.github.com>
2025-04-03 16:09:02 +0100

    Cycle through diagnostics levels

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -19,19 +19,34 @@ vim.api.nvim_set_keymap('n', 'q:', '<Nop>', { noremap = true, silent = true })
 keymap.set("n", "<localleader>h", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end,
     { desc = "Toggle inlay hints" })
 
-local function toggle_diagnostics_display()
-    local current_config = vim.diagnostic.config()
-    if current_config.virtual_lines then
+local function cycle_diagnostics_display()
+    local config = vim.diagnostic.config()
+    local current_vt = config.virtual_text == true
+    local current_vl = config.virtual_lines == true
+
+    if not current_vt and not current_vl then
         vim.diagnostic.config({
-            virtual_lines = false,
             virtual_text = true,
+            virtual_lines = false,
+
+        })
+    elseif current_vt and not current_vl then
+        vim.diagnostic.config({
+            virtual_text = false,
+            virtual_lines = true,
+        })
+    elseif not current_vt and current_vl then
+        vim.diagnostic.config({
+            virtual_text = false,
+            virtual_lines = false,
+
         })
     else
         vim.diagnostic.config({
-            virtual_lines = true,
             virtual_text = false,
+            virtual_lines = false,
         })
     end
 end
 
-vim.keymap.set("n", "<localleader>d", toggle_diagnostics_display, { desc = " Toggle diagnostics display mode" })
+vim.keymap.set("n", "<localleader>d", cycle_diagnostics_display, { desc = "Cycle diagnostics display (Off > Text > Lines)" })
