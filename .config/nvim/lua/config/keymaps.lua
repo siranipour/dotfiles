befe08b1c7bff37e25d54809167fec0864810aac
Shayan Iranipour <43517072+siranipour@users.noreply.github.com>
2025-04-04 16:26:54 +0100

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -19,34 +19,65 @@ vim.api.nvim_set_keymap('n', 'q:', '<Nop>', { noremap = true, silent = true })
 keymap.set("n", "<localleader>h", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end,
     { desc = "Toggle inlay hints" })
 
-local function cycle_diagnostics_display()
-    local config = vim.diagnostic.config()
-    local current_vt = config.virtual_text == true
-    local current_vl = config.virtual_lines == true
+-- Setup icons for diagnostics
+local diagnostic_icons = {
+    Error = " ",
+    Warn  = " ",
+    Hint  = " ",
+    Info  = " ",
+}
 
-    if not current_vt and not current_vl then
-        vim.diagnostic.config({
-            virtual_text = true,
-            virtual_lines = false,
+local signs = {
+    text = {
+        [vim.diagnostic.severity.ERROR] = diagnostic_icons.Error,
+        [vim.diagnostic.severity.WARN]  = diagnostic_icons.Warn,
+        [vim.diagnostic.severity.HINT]  = diagnostic_icons.Hint,
+        [vim.diagnostic.severity.INFO]  = diagnostic_icons.Info,
+    },
+}
 
-        })
-    elseif current_vt and not current_vl then
-        vim.diagnostic.config({
-            virtual_text = false,
-            virtual_lines = true,
-        })
-    elseif not current_vt and current_vl then
-        vim.diagnostic.config({
-            virtual_text = false,
-            virtual_lines = false,
+local diagnostic_configs = {
+    {
+        virtual_text = false,
+        virtual_lines = false,
+        underline = false,
+        signs = false,
 
-        })
+    },
+    {
+        virtual_text = false,
+        virtual_lines = false,
+        underline = true,
+        signs = signs,
+    },
+    {
+        virtual_text = true,
+        virtual_lines = false,
+        underline = true,
+        signs = signs,
+    },
+    {
+        virtual_text = false,
+        virtual_lines = true,
+        underline = true,
+        signs = signs,
+    }
+}
+
+_G.current_level = 1
+local function set_diagnostic(level)
+    _G.current_level = level
+    vim.diagnostic.config(diagnostic_configs[level])
+end
+
+local function increment_diagnostic()
+    local level = _G.current_level
+    if level + 1 > #diagnostic_configs then
+        set_diagnostic(1)
     else
-        vim.diagnostic.config({
-            virtual_text = false,
-            virtual_lines = false,
-        })
+        set_diagnostic(level + 1)
     end
 end
 
-vim.keymap.set("n", "<localleader>d", cycle_diagnostics_display, { desc = "Cycle diagnostics display (Off > Text > Lines)" })
+set_diagnostic(_G.current_level)
+vim.keymap.set("n", "<localleader>d", increment_diagnostic, { desc = "Cycle diagnostics display (Off > Lite > Text > Lines)" })
